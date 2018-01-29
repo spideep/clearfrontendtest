8341a6ea3062212dfa65bdcc7878412b00081ae1
Alexis Rengifo <alexisrengifo@gmail.com>
2018-01-29 09:56:28 -0500

    add service and consume it from component

--- reddit/src/app/app.module.ts
+++ reddit/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { JsonpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { NredditComponent } from './nreddit/nreddit.component';
     NredditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    JsonpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- reddit/src/app/nreddit/nreddit.component.ts
+++ reddit/src/app/nreddit/nreddit.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, Input } from '@angular/core';
+import { RedditDataService } from '../reddit-data.service';
+import { Post } from '../post';
+
 
 @Component({
   selector: 'app-nreddit',
   templateUrl: './nreddit.component.html',
-  styleUrls: ['./nreddit.component.css']
+  styleUrls: ['./nreddit.component.css'],
+  providers: [RedditDataService]
 })
 export class NredditComponent implements OnInit {
+	@Input('subreddit') subreddit:string;
 
-  constructor() { }
+	posts:Post[];
+
+  constructor( private data: RedditDataService ) { }
 
   ngOnInit() {
+  	this.data.fetchPosts(this.subreddit).subscribe(
+  		posts => this.posts = posts
+  		);
   }
-
 }
